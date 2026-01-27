856dbb55ad1fc5a9e938839bb704010c6c441205
Dadd0 <edoardo.coccio@outlook.it>
2026-01-28 00:35:11 +0100

    add minimal UI

---
+++ main.js
@@ -0,0 +1,53 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {useGeographic} from 'ol/proj.js';
+import {getLocation} from "./location.js"
+
+useGeographic();
+
+const coords = [12.4829321, 41.8933203]
+
+const markerFeature = new Feature({
+  geometry: new Point(coords)
+});
+
+const markerLayer = new VectorLayer({
+  source: new VectorSource({
+    features: [markerFeature]
+  }),
+  style: new Style({
+    image: new CircleStyle({
+      radius: 6,
+      fill: new Fill({ color: '#0b5f5a' }),
+      stroke: new Stroke({ color: '#ffffff', width: 2 })
+    })
+  })
+});
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    }),
+    markerLayer
+  ],
+  view: new View({
+    center: coords,
+    zoom: 20
+  })
+});
+
+getLocation().then((location) => {
+  if (!location) return;
+
+  map.getView().setCenter(location);
+  markerFeature.getGeometry().setCoordinates(location);
+})
